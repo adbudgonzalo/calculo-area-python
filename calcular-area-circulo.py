93bba48ce0c29cc3adf3bfcb93eab97fb81cf30e
Gonzalo Adbud <adbudgonzalo@gmail.com>
2024-08-24 18:21:02 -0300

    Realizamos una calculadora de área en Python

---
+++ calcular-area-circulo.py
@@ -0,0 +1,15 @@
+import math
+
+1.#Paso 1: Solicitar al usuario que ingrese el radio del circulo.
+
+radio= float(input("Por favor, ingrese el radio del circulo:"))
+
+2.#Paso 2: Calcular el área del círculo utilizando la fórmula area= π * radio^2 
+
+area= math.pi * (radio **2)
+
+
+3.#Paso 3: Mostrar al usuario el área calculada
+
+print ("El área del circulo con radio", radio, "es:", area)
+
